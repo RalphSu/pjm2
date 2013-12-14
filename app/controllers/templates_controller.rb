6a2c13229f139f879fd9a96172fa59be0f691983
jiangh <jhtina@126.com>
2013-12-13 23:54:23 -0500

    update template table

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,12 +1,17 @@
 class TemplatesController < ApplicationController
 	include TemplatesHelper
-
+	include NewsReleaseHelper
 	
 	layout 'admin'
 	
 	def index
 		## TODO: fix to get projects based on current user
-		@projects = Project.all
+		@groupClassifieds ={}
+		classifiedsNames= distinct_news_classifieds();
+		classifiedsNames.each do |classifiedsName|
+			 @groupClassifieds[classifiedsName] = find_new_classifieds(classifiedsName)
+		end
+		@groupClassifieds
 	end
 
 	def view_template
