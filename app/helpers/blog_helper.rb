332159db0a1bf955faebfe9edd097bbcbcce0950
Ralph Su <liasu@ebay.com>
2013-12-22 15:22:58 +0800

    for templates

--- app/helpers/blog_helper.rb
+++ app/helpers/blog_helper.rb
@@ -1,7 +1,17 @@
 module BlogHelper
+
 	def distinct_blog_classifieds() 
 		BlogClassified.all(:select => "DISTINCT(classified)").collect do |n|
 			n.classified
 		end
 	end
+
+	def find_blog_classifieds(classified)
+		BlogClassified.find(:all, :conditions => {:classified =>classified })
+	end
+
+	def distinct_blog_templates()
+		Template.find(:all, :conditions => {:template_type =>"博客类模板" })
+	end
+
 end
--- app/helpers/forum_helper.rb
+++ app/helpers/forum_helper.rb
@@ -1,7 +1,17 @@
 module ForumHelper
+
 	def distinct_forum_classifieds() 
-		WeiboClassified.all(:select => "DISTINCT(classified)").collect do |n|
+		ForumClassified.all(:select => "DISTINCT(classified)").collect do |n|
 			n.classified
 		end
 	end
+
+	def find_forum_classifieds(classified)
+		ForumClassified.find(:all, :conditions => {:classified =>classified })
+	end
+
+	def distinct_forum_templates()
+		Template.find(:all, :conditions => {:template_type =>"论坛类模板" })
+	end
+
 end
