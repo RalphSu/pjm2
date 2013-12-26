5a0161f64b398a58084b79087eab4288e7d1509e
Ralph Su <suliangfei@gmail.com>
2013-12-26 22:49:04 +0800

    remove useless rename weibo migration

--- db/schema.rb
+++ db/schema.rb
@@ -432,7 +432,6 @@ ActiveRecord::Schema.define(:version => 20131226141104) do
     t.string   "keywords_except"
     t.date     "end_time"
     t.date     "start_time"
-    t.string   "pj_keywords"
   end
 
   add_index "projects", ["lft"], :name => "index_projects_on_lft"
@@ -578,8 +577,8 @@ ActiveRecord::Schema.define(:version => 20131226141104) do
     t.string   "mail_notification",               :default => "",    :null => false
     t.string   "salt",              :limit => 64
     t.string   "phone_number"
-    t.boolean  "client"
     t.string   "user_type"
+    t.boolean  "client"
   end
 
   add_index "users", ["auth_source_id"], :name => "index_users_on_auth_source_id"
