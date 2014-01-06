20cd630226c0cc7b1510f0d79e13e943e77a13f9
Ralph Su <suliangfei@gmail.com>
2014-01-06 22:59:52 +0800

    fix typo in parameter

--- app/models/report_template.rb
+++ app/models/report_template.rb
@@ -1,7 +1,7 @@
 class ReportTemplate < ActiveRecord::Base
 	include Redmine::SafeAttributes
 
-	belong_to :project, :foreign_key='project_id'
+	belong_to :project, :foreign_key=>'project_id'
 
 	# manually maintianed the relationship between the template and the ***_classified
 	safe_attributes 'template_type', 'classified', 'position'
