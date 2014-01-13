868244e526c523c3d20770eba1977808dc8e7524
Ralph Su <liasu@ebay.com>
2014-01-13 17:45:28 +0800

    db migration for report task

---
+++ db/migrate/20140113084935_create_report_tasks.rb
@@ -0,0 +1,19 @@
+class CreateReportTasks < ActiveRecord::Migration
+  def self.up
+    create_table :report_tasks do |t|
+    	t.integer project_id,:null=>false
+    	t.string status
+    	t.datetime gen_start_time
+    	t.datetime gen_end_time
+    	t.datetime report_start_time
+    	t.datetime report_end_time
+    	t.string report_path
+    	t.integer gen_count
+		t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :report_tasks
+  end
+end
