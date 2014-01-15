40088c064d9e1feabdad166b099edd7f0a673a7a
jiangh <jhtina@126.com>
2014-01-15 09:06:20 -0500

    add summary and weixin template page

--- app/helpers/templates_helper.rb
+++ app/helpers/templates_helper.rb
@@ -5,7 +5,9 @@ module TemplatesHelper
 	            {:name => 'news', :action => :index, :partial => 'templates/news', :label => :label_news_template},
 	           	{:name => 'weibo', :action => :index, :partial => 'templates/weibo', :label => :label_weibo_template},
 	           	{:name => 'forum', :action => :index, :partial => 'templates/forum', :label => :label_forum_template},
-	           	{:name => 'blog', :action => :index, :partial => 'templates/blog', :label => :label_blog_template}
+	           	{:name => 'blog', :action => :index, :partial => 'templates/blog', :label => :label_blog_template},
+	           	{:name => 'weixin', :action => :index, :partial => 'templates/weixin', :label => :label_weixin_template},
+	           	{:name => 'summary', :action => :index, :partial => 'templates/summary', :label => :label_summary_template}
 	            ]
 	    
 	  end
